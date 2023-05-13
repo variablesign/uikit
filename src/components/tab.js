5ad82085db94e9a92b35ff727ad05adf521bddfd
Variable Sign <variablesign@gmail.com>
2023-05-13 10:25:49 +0000

    updated tabs

--- src/components/tab.js
+++ src/components/tab.js
@@ -3,7 +3,15 @@ import uk from '../uikit.js';
 import Component from '../component.js';
 
 const _component = 'tab';
-const _defaults = {};
+const _defaults = {
+    activeClass: null,
+    inactiveClass: null,
+    disabledClass: null,
+    hideClass: null,
+    animationStartClass: null,
+    animationEndClass: null,
+    transition: false
+};
 
 class Tab extends Component {
     constructor(element, config) {
